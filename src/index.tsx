0fbb2aba2671ea461b8241821ae587dfc10d39e7
Alexandre Negrel <alexandre@negrel.dev>
2024-06-16 12:11:01 +0200

    rewrite FAQ section to use details

--- src/index.tsx
+++ src/index.tsx
@@ -32,7 +32,7 @@ export const metas = {
 export default ({ comp }: Lume.Data) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="pt-16 px-4 sm:px-8 md:px-16 lg:px-32 mb-16 space-y-16 md:space-y-32 lg:space-y-48">
+      <main className="pt-16 px-4 mb-16 space-y-16 md:space-y-32 lg:space-y-48">
         <section className="w-full">
           <div className="max-w-6xl mx-auto gap-4 md:gap-8 px-4 sm:px-6 md:px-10 flex flex-col justify-center text-center -mt-8">
             <img
@@ -223,67 +223,12 @@ export default ({ comp }: Lume.Data) => {
             <comp.GithubIcon className="w-8 h-8" /> View source code
           </a>
         </section>
-        {
-          /*
-        <section className="w-full">
-          <h2 className="text-2xl md:text-3xl xl:text-4xl font-bold text-center mb-4 ">
-            Still not convinced?
-          </h2>
-          <p className="text-gray-700 dark:text-gray-300 text-center">
-            Checkout one of our comparison:
-          </p>
-
-          <div className="space-y-16 mt-8">
-            <img
-              src="/images/logo.jpg"
-              transform-images="jpg png avif webp"
-              className="aspect-square w-full h-auto max-w-32 mx-auto mb-8"
-            />
-            <p className="text-center font-bold text-6xl" aria-hidden="true">
-              VS
-            </p>
-            <div className="flex flex-wrap gap-16 justify-center items-center">
-              <a href="#todo">
-                <img
-                  transform-images="png avif webp"
-                  src="/images/google_analytics.png"
-                  className="h-24"
-                />
-                <span className="sr-only">
-                  Prisme Analytics vs Google Analytics
-                </span>
-              </a>
-              <a href="#todo">
-                <img
-                  transform-images="png avif webp"
-                  src="/images/plausible_logo.png"
-                  className="h-16"
-                />
-                <span className="sr-only">
-                  Prisme Analytics vs Plausible Analytics
-                </span>
-              </a>
-              <a href="#todo">
-                <img
-                  transform-images="png avif webp"
-                  src="/images/simpleanalytics.png"
-                  className="h-24"
-                />
-                <span className="sr-only">
-                  Prisme Analytics vs Simple Analytics
-                </span>
-              </a>
-            </div>
-          </div>
-        </section>
-          */
-        }{" "}
-        <section className="w-full">
-          <div class="mx-auto max-w-7xl relative">
-            <h2 className="text-center text-2xl md:text-3xl xl:text-4xl px-8 font-bold mb-8">
+        <section className="px-4 w-full max-w-7xl mx-auto flex flex-wrap justify-between gap-16">
+          <div className="flex-1 min-w-96">
+            <h2 className="text-2xl md:text-3xl xl:text-4xl font-bold mb-8">
               Frequently asked questions (and answers)
             </h2>
-            <p class="text-lg text-center">
+            <p class="text-lg">
               If you can’t find what you’re looking for, email us at{" "}
               <a
                 href="mailto:support@prismeanalytics.com"
@@ -293,92 +238,68 @@ export default ({ comp }: Lume.Data) => {
               </a>{" "}
               and we will get back to you.
             </p>
-            <ul
-              role="list"
-              class="list-none mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-12 lg:max-w-none lg:grid-cols-3"
+          </div>
+          <div className="flex-1 min-w-96 space-y-16">
+            <comp.Details
+              summary={
+                <h3 className="inline">
+                  What does Prisme Analytics do with my data?
+                </h3>
+              }
             >
-              <li>
-                <ul role="list" class="list-none flex flex-col gap-y-8">
-                  <li>
-                    <h3 class="text-lg">
-                      What does Prisme Analytics do with my data?
-                    </h3>
-                    <p class="mt-4 text-sm">
-                      We make it easily accessible to you, and that’s it. We
-                      don’t mine your data, sell it, use it for retargeting, or
-                      have any interest in it other than keeping it available
-                      for you. We sell our software, not data.
-                    </p>
-                  </li>
-                  <li>
-                    <h3 class="text-lg">
-                      How long does Prisme Analytics keep my data?
-                    </h3>
-                    <p class="mt-4 text-sm">
-                      Forever. Our data retention policy for all customers on
-                      all plans is that we never delete your data. Unlike most
-                      other analytics companies who keep your data for 30 days
-                      to 5 years (i.e. not long at all), we retain your data to
-                      view your dashboard forever. As long as you're a paying
-                      customer, your data is available to you forever.
-                    </p>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <ul role="list" class="list-none flex flex-col gap-y-8">
-                  <li>
-                    <h3 class="text-lg">
-                      How long does Prisme Analytics keep my data?
-                    </h3>
-                    <p class="mt-4 text-sm">
-                      Forever. Our data retention policy for all customers on
-                      all plans is that we never delete your data. Unlike most
-                      other analytics companies who keep your data for 30 days
-                      to 5 years (i.e. not long at all), we retain your data to
-                      view your dashboard forever. As long as you're a paying
-                      customer, your data is available to you forever.
-                    </p>
-                  </li>
-                  <li>
-                    <h3 class="font-display text-lg leading-7 text-slate-900">
-                      Do I need a cookie banner with Prisme Analytics?
-                    </h3>
-                    <p class="mt-4 text-sm">
-                      No. At least, not because of Prisme.
-                    </p>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <ul role="list" class="list-none flex flex-col gap-y-8">
-                  <li>
-                    <h3 class="font-display text-lg leading-7 text-slate-900">
-                      Is Prisme Analytics free?
-                    </h3>
-                    <p class="mt-4 text-sm">
-                      If you aren’t paying for the product, you are the product.
-                      Google Analytics is free, so the data you collect for them
-                      about your website visitors is the product. They’re an
-                      advertising company and make billions by knowing as much
-                      as possible about all of us. That’s why we charge a
-                      reasonable price for our software (we don’t have to or
-                      want to sell data).
-                    </p>
-                  </li>
-                  <li>
-                    <h3 class="text-lg">
-                      Can I cancel anytime?
-                    </h3>
-                    <p class="mt-4 text-sm">
-                      Yes. You can cancel your subscription at any moment. You
-                      will still have access to your account and data until the
-                      end of the period you paid for.
-                    </p>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+              We make it easily accessible to you, and that’s it. We don’t mine
+              your data, sell it, use it for retargeting, or have any interest
+              in it other than keeping it available for you. We sell our
+              software, not data.
+            </comp.Details>
+            <comp.Details
+              summary={
+                <h3 className="inline">
+                  How long does Prisme Analytics keep my data?
+                </h3>
+              }
+            >
+              Forever. Our data retention policy for all customers on all plans
+              is that we never delete your data. Unlike most other analytics
+              companies who keep your data for 30 days to 5 years (i.e. not long
+              at all), we retain your data to view your dashboard forever. As
+              long as you're a paying customer, your data is available to you
+              forever.
+            </comp.Details>
+            <comp.Details
+              summary={
+                <h3 className="inline">
+                  Is Prisme Analytics free?
+                </h3>
+              }
+            >
+              If you aren’t paying for the product, you are the product. Google
+              Analytics is free, so the data you collect for them about your
+              website visitors is the product. They’re an advertising company
+              and make billions by knowing as much as possible about all of us.
+              That’s why we charge a reasonable price for our software (we don’t
+              have to or want to sell data).
+            </comp.Details>
+            <comp.Details
+              summary={
+                <h3 className="inline">
+                  Can I cancel anytime?
+                </h3>
+              }
+            >
+              Yes. You can cancel your subscription at any moment. You will
+              still have access to your account and data until the end of the
+              period you paid for.
+            </comp.Details>
+            <comp.Details
+              summary={
+                <h3 className="inline">
+                  Do I need a cookie banner with Prisme Analytics?
+                </h3>
+              }
+            >
+              No, Prisme is GDPR, PECR and CCPA compliant.
+            </comp.Details>
           </div>
         </section>
       </main>
