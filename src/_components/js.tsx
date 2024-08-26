91b10cd227ea056e6e28cab750277f0798f950e8
Alexandre Negrel <alexandre@negrel.dev>
2024-08-26 16:24:49 +0200

    add instant.page script to reduce latency

--- src/_components/js.tsx
+++ src/_components/js.tsx
@@ -2,6 +2,13 @@ export default function () {
   return (
     <>
       <script src="/scripts/popover.js" defer async></script>
+      <script
+        src="//instant.page/5.2.0"
+        type="module"
+        integrity="sha384-jnZyxPjiipYXnSU0ygqeac2q7CVYMbh84q0uHVRRxEtvFPiQYbXWUorga2aqZJ0z"
+        defer
+      >
+      </script>
     </>
   );
 }
