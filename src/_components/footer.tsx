1e68850cd531f99e02119295531d46d18588df29
Alexandre Negrel <alexandre@negrel.dev>
2024-09-06 20:25:37 +0200

    move simple as last in footer

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,10 +1,10 @@
 const links = {
   "Why Prisme Analytics?": {
     "Open Source": "/is/open-source-website-analytics/",
-    "Simple": "/is/simple-analytics/",
     "Lightweight": "/is/lightweight-analytics/",
     "Privacy focused": "/is/privacy-focused-analytics/",
     "Progressive": "/is/progressive-analytics/",
+    "Simple": "/is/simple-analytics/",
     "For creators": "#todo",
     "For agencies": "#todo",
     "For ecommerce": "#todo",
